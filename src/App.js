c083f8567513f5da5544442167277b9c6a307850
Noeyeddeer <john.e.mades@gmail.com>
2022-04-11 15:54:06 -0700

    alert-index through bomb

--- src/App.js
+++ src/App.js
@@ -6,11 +6,37 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
+      board: ["?", "?", "?", "?", "?", "?", "?", "?", "?"],
+      treasureLocation: null,
+      bombLocation: null
+    }
+  }
+
+  componentDidMount() {
+    let treasure = Math.floor(Math.random()
+    * this.state.board.length)
+    let bomb = Math.floor(Math.random()
+    * this.state.board.length)
+    this.setState({treasureLocation: treasure, bombLocation: bomb})
+  }
+
+  handleGamePlay = (index) => {
+    const { board, treasureLocation, bombLocation } = this.state
+    if(index === treasureLocation) {
+      board[index] = "💎"
+      this.setState({board: board})
+    } else if(index === bombLocation) {
+      board[index] = "💣"
+      this.setState({board: board})
+    } else {
+    board[index] = "🌲"
+    this.setState({board: board})
     }
   }
 
   render() {
+    console.log(this.state.treasureLocation)
+    console.log(this.state.bombLocation)
     return(
       <>
         <h1>Treasure Hunt Game</h1>
@@ -20,6 +46,8 @@ class App extends Component {
           <Square 
             key={index}
             value={value}
+            index={index}
+            handleGamePlay={this.handleGamePlay}
           />
           )
         })}
